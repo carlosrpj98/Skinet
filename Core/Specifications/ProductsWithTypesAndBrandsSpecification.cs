41eec98b395d89e5647a2e44232445ca2ccb1afd
carlosrpj98 <kshadow100@gmail.com>
2023-08-14 22:06:01 -0300

    End of section 4

--- Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
+++ Core/Specifications/ProductsWithTypesAndBrandsSpecification.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Core.Entities;
 
 namespace Core.Specifications
@@ -9,6 +5,12 @@ namespace Core.Specifications
     public class ProductsWithTypesAndBrandsSpecification : BaseSpecifications<Product>
     {
         public ProductsWithTypesAndBrandsSpecification()
+        {
+            AddInclude(x => x.ProductType);
+            AddInclude(x => x.ProductBrand);
+        }
+
+        public ProductsWithTypesAndBrandsSpecification(int id) : base(x => x.Id == id)
         {
             AddInclude(x => x.ProductType);
             AddInclude(x => x.ProductBrand);
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -14,14 +14,14 @@ namespace Infrastructure.Data
         ISpecification<TEntity> spec)
         {
             var query = inputQuery;
-            
+
             if (spec.Criteria != null)
             {
                 query = query.Where(spec.Criteria);
             }
-            
-            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
 
+            query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
+            
             return query;
         }
     }
